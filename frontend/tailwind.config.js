16708d6ce415da05ac1803b99009f6d91e8afb85
Ayman el founti <aymaneelfounti@gmail.com>
2026-05-05 18:23:17 +0100

    Completing Home and About Pages

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,ts,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#2563EB',
+        secondary: '#1E40AF',
+        dark: '#0F172A',
+        'bg-light': '#F0F4FF',
+        'bg-section': '#EEF2FF',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
