83f6895fd70afbcc9259285c95c6201aaba524c7
mariame <mariame.baala@gmail.com>
2026-05-17 04:41:44 +0100

    complite profile pages

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,9 +2,10 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './features/public/home/home.component';
 import { AboutComponent } from './features/public/about/about.component';
 import { LoginComponent } from './features/public/auth/login/login.component';
+import { RegisterComponent } from './features/public/auth/register/register.component'; 
 import { NutritionComponent } from './features/public/nutrition/nutrition.component';
 import { ClubsComponent } from './features/public/clubs/clubs.component';
-import { RegisterComponent } from './features/public/auth/register/register.component';
+import { ProfilesComponent } from './features/public/profiles/profiles.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -13,6 +14,11 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent }, 
   { path: 'nutrition', component: NutritionComponent },
   { path: 'clubs', component: ClubsComponent },
+  
+  
+  { path: 'complete-profile', component: ProfilesComponent },
+  { path: 'profiles', component: ProfilesComponent },
 
+  
   { path: '**', redirectTo: '' }
 ];
