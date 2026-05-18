cbaf526df911c38406fd74a1f74893991bd2045d
Emy-gold <imanelgarate@gmail.com>
2026-05-18 21:30:19 +0100

    add the client response

---
+++ backend/src/main/java/org/smarttrainer/backend/modules/client/dto/ClientProfileResponse.java
@@ -0,0 +1,23 @@
+package org.smarttrainer.backend.modules.client.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class ClientProfileResponse {
+
+    private Long id;
+    private LocalDateTime createAt;
+    private Long userId;
+    private int age;
+    private Double poids;
+    private Double taille;
+    private String niveau;
+    private Double imc;
+    private Long coachID;
+    private Long clubId;
+
+}
