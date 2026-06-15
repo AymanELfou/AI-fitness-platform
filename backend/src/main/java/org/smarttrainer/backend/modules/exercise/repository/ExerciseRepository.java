8cb681b072270a398520fa7cdf8489c0b37a6899
Emy-gold <imanelgarate@gmail.com>
2026-06-16 00:06:19 +0100

    add the repo

--- backend/src/main/java/org/smarttrainer/backend/modules/exercise/repository/ExerciseRepository.java
+++ backend/src/main/java/org/smarttrainer/backend/modules/exercise/repository/ExerciseRepository.java
@@ -3,12 +3,17 @@ package org.smarttrainer.backend.modules.exercise.repository;
 import org.smarttrainer.backend.domain.exercice.CreatedByRole;
 import org.smarttrainer.backend.domain.exercice.Difficulty;
 import org.smarttrainer.backend.domain.exercice.Exercise;
+import org.smarttrainer.backend.domain.user.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
     List<Exercise> findByDifficulty(Difficulty difficulty);
     List<Exercise> findByMusclesGroup(String musclesGroup);
-    List<Exercise> findByCreatedByRole(CreatedByRole createdByRole);
+
+    @Query("SELECT e FROM Exercise e WHERE e.createdBy IN (SELECT u.id FROM User u JOIN u.roles r WHERE r = :role)")
+    List<Exercise> findExercisesByCreatorRole(@Param("role") Role role);
 }
