f67c985d42647b9cf1edc529578139b0e29469a1
Emy-gold <imanelgarate@gmail.com>
2026-06-21 14:03:43 +0100

    add the mapper of post

---
+++ backend/src/main/java/org/smarttrainer/backend/modules/post/mapper/PostMapper.java
@@ -0,0 +1,29 @@
+package org.smarttrainer.backend.modules.post.mapper;
+
+import org.smarttrainer.backend.domain.post.Post;
+import org.smarttrainer.backend.modules.post.dto.PostRequest;
+import org.smarttrainer.backend.modules.post.dto.PostResponse;
+import org.springframework.stereotype.Component;
+
+@Component
+public class PostMapper {
+
+    public PostResponse toResponse(Post post) {
+        return PostResponse.builder()
+                .id(post.getId())
+                .createdAt(post.getCreatedAt())
+                .content(post.getContent())
+                .communityId(post.getCommunity() != null ? post.getCommunity().getId() : null)
+                .userId(post.getUser() != null ? post.getUser().getId() : null)
+                .userName(post.getUser() != null ? post.getUser().fullName() : null)
+                .commentsCount(post.getComments() != null ? post.getComments().size() : 0)
+                .likesCount(post.getLikes() != null ? post.getLikes().size() : 0)
+                .build();
+    }
+
+    public Post toEntity(PostRequest request) {
+        Post post = new Post();
+        post.setContent(request.getContent());
+        return post;
+    }
+}
