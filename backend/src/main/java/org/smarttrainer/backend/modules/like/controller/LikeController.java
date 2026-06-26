53ffb7cc1b1d6cab29552a74b709c6183943f455
Emy-gold <imanelgarate@gmail.com>
2026-06-26 14:56:03 +0100

    add the controller  metier

---
+++ backend/src/main/java/org/smarttrainer/backend/modules/like/controller/LikeController.java
@@ -0,0 +1,34 @@
+package org.smarttrainer.backend.modules.like.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.smarttrainer.backend.modules.like.dto.LikeResponse;
+import org.smarttrainer.backend.modules.like.service.LikeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/likes")
+@RequiredArgsConstructor
+public class LikeController {
+
+    private final LikeService service;
+
+    @PostMapping("toggle/{postId}/{userId}")
+    public ResponseEntity<String> toggle(
+            @PathVariable Long postId,
+            @PathVariable Long userId) {
+        return ResponseEntity.ok(service.toggle(postId, userId));
+    }
+
+    @GetMapping("post/{postId}")
+    public ResponseEntity<List<LikeResponse>> getByPostId(@PathVariable Long postId) {
+        return ResponseEntity.ok(service.getByPostId(postId));
+    }
+
+    @GetMapping("post/{postId}/count")
+    public ResponseEntity<Integer> countByPostId(@PathVariable Long postId) {
+        return ResponseEntity.ok(service.countByPostId(postId));
+    }
+}
